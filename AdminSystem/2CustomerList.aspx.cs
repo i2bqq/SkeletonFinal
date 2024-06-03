78cc7871b9b3241e6ff32b3767aedfdfd48a753a
P2773421 <P2773421@my365.dmu.ac.uk>
2024-06-03 18:02:22 +0100

    12

--- AdminSystem/2CustomerList.aspx.cs
+++ AdminSystem/2CustomerList.aspx.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (IsPostBack == false)
+        {
+
+            DisplayCustomers();
+        }
+    }
+
+
+    void DisplayCustomers()
+    {
+
+        ClsCustomerCollection Customer = new ClsCustomerCollection();
+        lstCustomerList.DataSource = Customer.CustomerList;
+        lstCustomerList.DataValueField = "CustomerID";
+        lstCustomerList.DataTextField = "FirstName";
+        lstCustomerList.DataBind();
+        
+
 
     }
 }

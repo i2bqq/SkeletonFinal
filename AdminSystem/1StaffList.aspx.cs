c8c782037a28694d6a82d01b38191ccf0fd34d20
P2744901 <P2744901@my365.dmu.ac.uk>
2024-06-04 02:18:33 +0100

    Staff delete and filter Method Updated

--- AdminSystem/1StaffList.aspx.cs
+++ AdminSystem/1StaffList.aspx.cs
@@ -50,4 +50,40 @@ public partial class _1_List : System.Web.UI.Page
             lblError.Text = " Please select a record from the list to edit";
         }
     }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        Int32 StaffID;
+        if (lstStaffList.SelectedIndex != -1)
+        {
+            StaffID = Convert.ToInt32(lstStaffList.SelectedValue);
+            Session["StaffID"] = StaffID;
+            Response.Redirect("1StaffConfirmDelete.aspx");
+        }
+        else
+        {
+            lblError.Text = "Please select a record from the list to delete";
+        }
+    }
+
+    protected void btnApplyFilter_Click(object sender, EventArgs e)
+    {
+        clsSatffCollection AnStaff = new clsSatffCollection();
+        AnStaff.ReportByStaffID(txtEnterStaffID.Text);
+        lstStaffList.DataSource = AnStaff.StaffList;
+        lstStaffList.DataTextField = "StaffID";
+        lstStaffList.DataTextField = "FirstName";
+        lstStaffList.DataBind();
+    }
+
+    protected void btnClearFilter_Click(object sender, EventArgs e)
+    {
+        clsSatffCollection AnStaff = new clsSatffCollection();
+        AnStaff.ReportByStaffID("");
+        txtEnterStaffID.Text = "";
+        lstStaffList.DataSource = AnStaff.StaffList;
+        lstStaffList.DataTextField = "StaffID";
+        lstStaffList.DataTextField = "FirstName";
+        lstStaffList.DataBind();
+    }
 }
